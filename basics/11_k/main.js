159ad35caf6d66c7f6097d432bb0d85e15c9ebc4
muskan-chouhan <mushkanc430@gmail.com>
2025-12-10 16:38:31 +0530

    Add 11 file

---
+++ basics/11_k/main.js
@@ -0,0 +1,7 @@
+
+let filename = "system.php";
+console.log(filename.split('.').pop());
+
+// Reassign the variable filename and log the file extension for the new value
+filename = "abc.js";
+console.log(filename.split('.').pop());
