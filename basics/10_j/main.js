63906af622104dbb80900d6a3d543be5666eb770
muskan-chouhan <mushkanc430@gmail.com>
2025-12-10 16:18:15 +0530

    Add 10 file

---
+++ basics/10_j/main.js
@@ -0,0 +1,13 @@
+
+
+// User se variable ka naam lo
+let userVarName = prompt("Enter variable name:");
+
+// Ek empty object banao
+let obj = {};
+
+// User ke diye naam se variable (property) banao
+obj[userVarName] = "Hello World";
+
+// Output show karo
+console.log(obj[userVarName]);
