7038c653c30735c0ac5003875d77dcbbe3ee41b3
muskan-chouhan <mushkanc430@gmail.com>
2025-12-17 15:46:26 +0530

    Create String of Specified Copies

--- basics/51/main.js
+++ basics/51/main.js
@@ -10,3 +10,16 @@ function repeatString(str, n) {
 
 console.log(repeatString("hi", 3));      // hihihi
 console.log(repeatString("muskan", 2));  // muskanmuskan
+
+
+///
+function string_copies (str, n) 
+{
+  if (n < 0)
+    return false;
+  else
+  return str.repeat(n);
+}
+console.log(string_copies("abc", 5));
+console.log(string_copies("abc", 0));
+console.log(string_copies("abc", -2));
