62f58bd40c967d9db37270c58d08e3a5c09f9ff1
muskan-chouhan <mushkanc430@gmail.com>
2025-12-18 15:40:00 +0530

    Remove First/Last 'P' Characters in String

---
+++ basics/60/main.js
@@ -0,0 +1,22 @@
+function city_name(str) {
+    if (str.slice(0,3) === "Los" || str.slice(0,3) === "New") {
+        return str;
+    }
+    return "";   // blank
+}
+
+console.log(city_name("New York"));     // New York
+console.log(city_name("Los Angeles"));  // Los Angeles
+console.log(city_name("London"));       // ""
+
+
+//
+function city_name(str) {
+  if (
+      str.length >= 3 &&
+      (str.substring(0, 3) == 'Los' || str.substring(0, 3) == 'New')
+     ) {
+      return str;
+  }
+  return '';
+}
