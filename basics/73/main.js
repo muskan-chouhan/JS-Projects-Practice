f549bd837b926a2756789f7907db3dd3fb4dba5b
muskan-chouhan <mushkanc430@gmail.com>
2026-01-01 18:37:06 +0530

    Check if Array Does Not Contain 1 or 3

--- basics/73/main.js
+++ basics/73/main.js
@@ -1,5 +1,5 @@
-const check = (arr) => {
-        return arr.includes(1) || arr.includes(3);
+const check = (nums) => {
+        return !nums.includes(1) && !nums.includes(3);
 }
 
 console.log(check([2,3]));
